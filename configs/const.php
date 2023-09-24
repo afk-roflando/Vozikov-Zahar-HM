1bd4decda8540b975229b988472d78fe45933b3a
Vozikov Zhar <zvozikov@gmail.com>
2023-09-24 13:01:16 +0300

    new

--- configs/const.php
+++ configs/const.php
@@ -1,9 +1,11 @@
 <?php
 
-const DB_HOST = 'localhost';
+const DB_HOST = 'base';
 const DB_USER = 'root';
 const DB_PASSWORD = 'KJKszpj';
 
+const DATABASE = 'PS_shop';
+const DSN = 'mysql:host= ' . DB_HOST . ';dbname='. DATABASE ;
 const APP_DIR = BASE_DIR .'/app/';
 
 const VIEW_DIR = BASE_DIR . '/views/';
@@ -12,9 +14,8 @@ const PARTS_DIR = VIEW_DIR .'/parts/';
 
 define('DOMAIN', $_SERVER ['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
 
-const ASSETS_URI = DOMAIN . '/assets/';
-const ASSETS_DIR = BASE_DIR . '/assets/';
+const ASSETS_URI = DOMAIN . '/assets';
+const ASSETS_DIR = BASE_DIR . '/assets';
 
-const IMAGES_URI = ASSETS_URI . '/img';
-const IMAGES_DIR = ASSETS_DIR . '/img';
-
+const IMAGES_URI = ASSETS_URI . 'img';
+const IMAGES_DIR = ASSETS_DIR . 'img';
--- index.php
+++ index.php
@@ -12,10 +12,21 @@ error_reporting(E_ALL);
 
 
 try {
-   require_once APP_DIR . 'index.php';
+    require_once BASE_DIR . '/configs/DB.php';
+    require_once APP_DIR . 'index.php';
 
+
+    $query = 'SELECT * FROM content';
+    $query = DB::connect()->prepare($query);
+    $query -> execute();
+    dd($query->fetchAll());
     require_once BASE_DIR . '/configs/router.php';
 
-} catch (Exception $exception) {
-    d($exception ->getCode() . ' - "' . $exception->getMessage() . '"');
+} catch (PDOException $exception) {
+    d('PDOException');
+    dd($exception ->getCode() . ' - "' . $exception->getMessage() . '"');
+}
+
+catch (Exception $exception) {
+    dd($exception ->getCode() . ' - "' . $exception->getMessage() . '"');
 }
